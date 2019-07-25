7db9593b9fa2978b4ab41fa429a709ce0ae843d1
Mahdi Bakhshi <mbkhsh1996@gmail.com>
2019-07-25 15:54:30 +0200

    Add stop condition for testing
    Some prints are also added

--- model/tlc.go
+++ model/tlc.go
@@ -1,11 +1,15 @@
 package model
 
+import "fmt"
+
 // Advance will change the step of node to a new one.
 func (node *Node) Advance(step int) {
 	node.TimeStep = step
 	node.Acks = 0
 
+	fmt.Printf("node %d , Broadcast in timeStep %d\n", node.Id, node.TimeStep)
 	node.Comm.Broadcast(node.makeMsg(node.Id, step, node.History))
+
 }
 
 // makeMsg makes a new message to be sent to other nodes.
@@ -20,10 +24,17 @@ func (node *Node) makeMsg(source int, step int, history []Message) *Message {
 
 // waitForMsg endlessly(?) (till a stop point actually) waits for upcoming messages and then decides the next action with respect to msg's contents.
 func (node *Node) WaitForMsg(stop int) {
-	for node.TimeStep < stop {
+	for node.TimeStep <= stop {
 		// For now we assume that the underlying receive function is blocking
 		// TODO Implement receive in a blocking way or introduce 2 kinds of receives
 		msg := node.Comm.Receive()
+		fmt.Printf("node %d,%d\n", node.Id, msg.Step)
+
+		if node.TimeStep == stop {
+			fmt.Println("Break reached")
+
+			break
+		}
 
 		if msg.Step > node.TimeStep { // Node needs to catch up with the message
 			// Update nodes local history. Append history from message to local history
