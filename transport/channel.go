a009f65358f3a449935cde27d8f2be8a217355fd
Mahdi Bakhshi <mbkhsh1996@gmail.com>
2019-07-25 15:53:53 +0200

    Fix some pointer reference problems
    Also some prints are added to trace the execution.

--- transport/channel.go
+++ transport/channel.go
@@ -1,27 +1,36 @@
 package transport
 
-import "../model"
+import (
+	"../model"
+	"fmt"
+)
 
 type Channel struct {
-	outgoingChannels map[int]chan *model.Message
-	incomingChannel  chan *model.Message
+	outgoingChannels *map[int]*chan *model.Message
+	incomingChannel  *chan *model.Message
 }
 
 // Send is used for sending a message i
 func (c *Channel) Broadcast(msg *model.Message) {
 	go func() {
-		for _, peerChannel := range c.outgoingChannels {
-			peerChannel <- msg
+		for _, peerChannel := range *c.outgoingChannels {
+			select {
+			case *peerChannel <- msg:
+			default:
+				fmt.Printf("channel full, msg : %v\n", msg.Source)
+			}
 		}
 	}()
 }
 
 func (c *Channel) Send(msg *model.Message, n *model.Node) {
 	go func() {
-		c.outgoingChannels[n.Id] <- msg
+		*(*c.outgoingChannels)[n.Id] <- msg
 	}()
 }
 
 func (c *Channel) Receive() *model.Message {
-	return <-c.incomingChannel
+	msg := <-*(c.incomingChannel)
+	return msg
+	//return <-*c.incomingChannel
 }
