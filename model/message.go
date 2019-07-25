6e4357fc3217b84ab4c78a8d54dbea8a533caeb5
Mahdi Bakhshi <mbkhsh1996@gmail.com>
2019-07-25 12:23:03 +0200

    Add model for node and message

---
+++ model/message.go
@@ -0,0 +1,14 @@
+package model
+
+type MsgType int
+
+const (
+	Raw = iota
+)
+
+type Message struct {
+	Source  int       // NodeID of message's source
+	Step    int       // Time step of message
+	MsgType MsgType   // Type of message
+	History []Message // History of messages. Sent to ensure causality, But in an inefficient way.
+}
---
+++ model/node.go
@@ -0,0 +1,17 @@
+package model
+
+type Node struct {
+	Id        int                    // Id of the node
+	TimeStep  int                    // Node's local time step
+	Threshold int                    // Threshold on number of messages
+	Acks      int                    // Number of acknowledges
+	Comm      CommunicationInterface // interface for communicating with other nodes
+	History   []Message              // History of received messages by a node
+
+}
+
+type CommunicationInterface interface {
+	Send(msg *Message, n *Node) // Send a message to a specific node
+	Broadcast(msg *Message)     // Broadcast messages to other nodes
+	Receive() *Message          // Blocking(?) receive
+}
